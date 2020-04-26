32533cfda8a84d3607bc29aa9b433ce0f6c66a57
Cameron Bulock <cbulock@liquidweb.com>
2020-04-26 17:50:26 -0400

    fix path resolution

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-var fs = require('fs');
-module.exports = fs.readFileSync('hosts', 'utf8');
+const fs = require('fs');
+const path = require('path');
+module.exports = fs.readFileSync(path.resolve(__dirname, './hosts'), 'utf8');
