01c7e0056f9f813f935b17a3a29cbe2392d7cdda
Cameron Bulock <cameron@bulock.com>
2020-06-07 18:16:22 -0400

    remove debugging

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 const fs = require('fs');
 const path = require('path');
-console.log('Hosts path: ', path.resolve(__dirname, './hosts'));
 module.exports = fs.readFileSync(path.resolve(__dirname, './hosts'), 'utf8');
