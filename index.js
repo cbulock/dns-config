524c12146816e397dfbcd4e444a5198145f539e9
Cameron Bulock <cameron@bulock.com>
2020-06-07 18:04:11 -0400

    debugging

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 const path = require('path');
-console.log(path.resolve(__dirname, './hosts'));
+console.log('Hosts path: ', path.resolve(__dirname, './hosts'));
 module.exports = fs.readFileSync(path.resolve(__dirname, './hosts'), 'utf8');
